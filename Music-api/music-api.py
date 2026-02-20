b0520225690ec9765613966963935dc94a7cd193
Emily <emilymope13@gmail.com>
2026-02-20 18:06:06 -0500

    Corrijo ruta principal con index.html

--- Music-api/music-api.py
+++ Music-api/music-api.py
@@ -2,17 +2,14 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-# Datos de ejemplo
 songs = ["Song A", "Song B", "Song C"]
 artists = ["Artist 1", "Artist 2"]
 albums = ["Album X", "Album Y"]
 
-# Página principal con HTML dinámico
 @app.route("/")
 def home():
     return render_template("index.html", songs=songs, artists=artists, albums=albums)
 
-# Endpoints JSON opcionales
 @app.route("/songs")
 def get_songs():
     return jsonify(songs)
